2338e209760c42e0d6ab05ce9f9b6ded13559342
lucas rodriguez <lucasrodriguez805@gmail.com>
2018-03-14 22:30:50 +0000

    I added things

--- js/script.js
+++ js/script.js
@@ -1,10 +1,18 @@
 // *************************** YOUR CODE BELOW *******************************
 //******************TEST EARLY AND OFTEN USING console.log() ******************
 //****************** SERIOUSLY TEST USING console.log()!!! ******************
+/* global $ */
 
 $(document).ready(function(){
-  
-  
-  
-  
+    $.ajax({
+        url: "https://api.giphy.com/v1/stickers/search?q=dog&api_key=dc6zaTOxFJmzC",
+        method: "GET",
+        success: function(response) {
+            var img = response.data[1].images.original.url;
+            console.log(img);
+
+            $('body').append('<img src=' + img + '>');
+
+        },
+    });
 });
