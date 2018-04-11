0708376b04b67c080e1da21547d2f2fb760a82be
lucas rodriguez <lucasrodriguez805@gmail.com>
2018-04-11 22:26:13 +0000

    completed fisheys

--- js/script.js
+++ js/script.js
@@ -3,16 +3,30 @@
 //****************** SERIOUSLY TEST USING console.log()!!! ******************
 /* global $ */
 
-$(document).ready(function(){
-    $.ajax({
-        url: "https://api.giphy.com/v1/stickers/search?q=dog&api_key=dc6zaTOxFJmzC",
-        method: "GET",
-        success: function(response) {
-            var img = response.data[1].images.original.url;
-            console.log(img);
+$(document).ready(function() {
+    $(".btn").click(function() {
 
-            $('body').append('<img src=' + img + '>');
+        // get whatever is in the input field
+        var search = $("#srch-term").val();
+        console.log(search);
 
-        },
+        // create the url that we want to send to giphy
+        var url = "https://api.giphy.com/v1/stickers/search?q=" + search + "&api_key=dc6zaTOxFJmzC"
+        console.log(url);
+        // get the response back from giphy
+        
+        $.ajax({
+            url: url,
+            method: "GET",
+            success: function(response) {
+                var img = response.data[1].images.original.url;
+                console.log(img);
+
+                $('body').append('<img src=' + img + '>');
+            },
+        });
+        // $('body').append('<img src=' + img + '>');// render the image returns on the page
+
+
     });
 });
